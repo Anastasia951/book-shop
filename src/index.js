7192aebc36e8d2a9872ab843b8fa22d60e2449f9
Anastasia951 <n.ivanova504@gmail.com>
2023-02-14 23:47:24 +0300

    grid in header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import 'normalize.css'
 import './styles/main.scss'
 import './styles/header.scss'
+import './styles/main-block.scss'
+
+
+function clickHandler() {
+  list.classList.toggle('visible')
+  burger.classList.toggle('closed')
+}
+
+const list = document.querySelector("[data-list]")
+const burger = document.querySelector("[data-burger]")
+
+burger.addEventListener('click', clickHandler)
