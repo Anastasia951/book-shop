12c036c44c67767964a3abc371266f6b7ebe2591
Anastasia951 <n.ivanova504@gmail.com>
2023-02-17 19:56:30 +0300

    adaptive main-block + header

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './styles/main-block.scss'
 function clickHandler() {
   list.classList.toggle('visible')
   burger.classList.toggle('closed')
+  document.body.classList.toggle('lock')
 }
 
 const list = document.querySelector("[data-list]")
